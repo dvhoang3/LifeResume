17fd75aa7439aa8eae3b60620fa1ebfc9f30bd4e
David Hoang <dhoangvie@gmail.com>
2025-04-16 20:43:20 -0500

    added fonts to select for FontDropdown

--- client/src/components/ResumeEditor/FontDropdown/FontDropdown.tsx
+++ client/src/components/ResumeEditor/FontDropdown/FontDropdown.tsx
@@ -1,54 +1,52 @@
 import { Select } from "@base-ui-components/react";
 import { FaAngleDown, FaCheck } from "react-icons/fa";
+import styles from './FontDropdown.module.css'
+
+const fontOptions: string[] = [
+  'Calibri',
+  'Arial',
+  'Cambria',
+  'Times New Roman',
+  'Garamond',
+  'Trebuchet MS',
+  'Book Antiqua',
+  'Arial Narrow',
+  'Georgia',
+  'Helvetica',
+  'Verdana',
+];
 
 function FontDropdown() {
+
   return (
     <>
-     <Select.Root>
-      <Select.Trigger>
-        <Select.Value placeholder="Sans-serif" />
-        <Select.Icon>
-          <FaAngleDown />
-        </Select.Icon>
-      </Select.Trigger>
+      <Select.Root>
+        <Select.Trigger className={styles.trigger}>
+          <Select.Value />
+          <Select.Icon>
+            <FaAngleDown size={12} />
+          </Select.Icon>
+        </Select.Trigger>
 
-      <Select.Portal>
-        <Select.Positioner sideOffset={8}>
-          <Select.ScrollUpArrow />
-          <Select.Popup>
-            <Select.Item value="sans">
-              <Select.ItemIndicator>
-                <FaCheck />
-              </Select.ItemIndicator>
-              <Select.ItemText>
-                Sans-serif
-              </Select.ItemText>
-            </Select.Item>
-            <Select.Item value="serif">
-              <Select.ItemIndicator>
-                <FaCheck />
-              </Select.ItemIndicator>
-              <Select.ItemText>Serif</Select.ItemText>
-            </Select.Item>
-            <Select.Item value="mono">
-              <Select.ItemIndicator>
-                <FaCheck />
-              </Select.ItemIndicator>
-              <Select.ItemText>
-                Monospace
-              </Select.ItemText>
-            </Select.Item>
-            <Select.Item value="cursive">
-              <Select.ItemIndicator>
-                <FaCheck />
-              </Select.ItemIndicator>
-              <Select.ItemText>Cursive</Select.ItemText>
-            </Select.Item>
-          </Select.Popup>
-          <Select.ScrollDownArrow />
-        </Select.Positioner>
-      </Select.Portal>
-     </Select.Root>
+        <Select.Portal>
+          <Select.Positioner sideOffset={8}>
+            <Select.ScrollUpArrow />
+            <Select.Popup>
+              {fontOptions.map((font: string) =>
+                <Select.Item value={font} key={font}>
+                  <Select.ItemIndicator>
+                    <FaCheck />
+                  </Select.ItemIndicator>
+                  <Select.ItemText>
+                    {font}
+                  </Select.ItemText>
+                </Select.Item>
+              )}
+            </Select.Popup>
+            <Select.ScrollDownArrow />
+          </Select.Positioner>
+        </Select.Portal>
+      </Select.Root>
     </>
   )
 }
